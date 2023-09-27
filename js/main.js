8edb35fbb85dbd606835d3218b9e2221ed1bd2fd
quandiqdev <quandiqdev@gmail.com>
2023-09-27 06:41:24 +0500

    slider-added

--- js/main.js
+++ js/main.js
@@ -19,16 +19,29 @@ try {
   });
 } catch (e) {}
 
-// try {
-//   var advantagesSlider = new Swiper(".advantages__slider", {
-//     slidesPerView: 3,
-//     spaceBetween: 24,
-//     pagination: {
-//       el: ".swiper-pagination",
-//       clickable: true,
-//     },
-//   });
-// } catch (e) {}
+try {
+  var advantagesSlider = new Swiper(".advantages__slider", {
+    pagination: {
+      el: ".advantages__slider-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+        autoHeight: true,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 24,
+      },
+      992: {
+        slidesPerView: 3,
+        spaceBetween: 24,
+      },
+    },
+  });
+} catch (e) {}
 
 try {
   //arguments accordeon
@@ -81,17 +94,108 @@ try {
 } catch (e) {}
 
 try {
+  //header fixed
   let lastScrollTop = 0;
   let navbar = document.querySelector(".header");
-  window.addEventListener("scroll", function () {
+  const mobileScreenWidth = 1024;
+
+  function toggleNavbarVisibility() {
     let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    if (scrollTop > lastScrollTop) {
-      navbar.style.top = "-80px";
+    if (window.innerWidth <= mobileScreenWidth) {
+      navbar.style.opacity = scrollTop === 0 ? "0" : "1";
+      navbar.style.visibilty = scrollTop === 0 ? "hidden" : "visible";
+      navbar.style.zIndex = scrollTop === 0 ? -1 : 999;
+
+      if (scrollTop > lastScrollTop) {
+        navbar.style.top = "-80px";
+      } else {
+        navbar.style.top = "0";
+        navbar.style.position = "fixed";
+        navbar.style.background = scrollTop === 0 ? "transparent" : "#212121";
+      }
+    } else if (scrollTop > lastScrollTop) {
+      navbar.style.opacity = "0";
+      navbar.style.visibilty = "hidden";
+      navbar.style.zIndex = -1;
     } else {
-      navbar.style.top = "0";
+      navbar.style.opacity = "1";
+      navbar.style.visibilty = "visible";
+      navbar.style.zIndex = 9999;
       navbar.style.position = "fixed";
-      navbar.style.background = "#212121";
+      navbar.style.background = scrollTop === 0 ? "transparent" : "#212121";
     }
     lastScrollTop = scrollTop;
+  }
+
+  toggleNavbarVisibility();
+  window.addEventListener("scroll", toggleNavbarVisibility);
+  window.addEventListener("resize", toggleNavbarVisibility);
+} catch (e) {}
+
+try {
+  const connectionBtn = document.querySelector(".intro__connection-btn");
+  const introSocial = document.querySelector(".intro__social");
+  const introSocialClose = document.querySelector(".intro__social-close");
+
+  connectionBtn.addEventListener("click", () => {
+    introSocial.classList.add("intro__social--active");
+  });
+
+  introSocialClose.addEventListener("click", () => {
+    introSocial.classList.remove("intro__social--active");
   });
 } catch (e) {}
+
+try {
+  let init = false;
+  let faqSLider;
+  function swiperCard() {
+    if (window.innerWidth <= 768) {
+      if (!init) {
+        init = true;
+        faqSLider = new Swiper(".faq__slider", {
+          slidesPerView: 1,
+          autoHeight: false,
+
+          pagination: {
+            el: ".faq__slider-pagination",
+            clickable: true,
+          },
+        });
+      }
+    } else if (init) {
+      faqSLider.destroy();
+      init = false;
+    }
+  }
+  swiperCard();
+  window.addEventListener("resize", swiperCard);
+} catch (e) {}
+
+try {
+  const choiceSlider = new Swiper(".choice__slider", {
+    slidesPerView: 1,
+    autoHeight: true,
+    pagination: {
+      el: ".choice__slider-pagination",
+      clickable: true,
+    },
+  });
+} catch (error) {}
+
+try {
+  const benefitsSlider = new Swiper(".benefits__slider", {
+    pagination: {
+      el: ".benefits__slider-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+    },
+  });
+} catch (error) {}
